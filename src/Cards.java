262d4ed68937ac96fa72d5522a10ac17a7bdc585
Rarepug1st <99208797+Rarepug1st@users.noreply.github.com>
2022-12-05 14:40:22 -0500

    q

---
+++ src/Cards.java
@@ -0,0 +1,20 @@
+
+public class Cards {
+
+	
+	String Card1 = "Ace";
+	String Card2 = "1";
+	String Card3 = "2";
+	String Card4 = "3";
+	String Card5 = "4";
+	String Card6 = "6";
+	String Card7 = "7";
+	String Card8 = "8";
+	String Card9 = "9";
+	String Card10 = "10";
+	String Card11 = "Jack";
+	String Card12 = "Queen";
+	String card13 = "King";
+	
+	
+}
