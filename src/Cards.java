f7842744cf1d29ef631df4c3ce8608b2e885fdbd
Rarepug1st <99208797+Rarepug1st@users.noreply.github.com>
2022-12-06 14:31:14 -0500

    ao

--- src/Cards.java
+++ src/Cards.java
@@ -1,20 +1,29 @@
 
 public class Cards {
+	private String name;
+	// Lets use 52 Card
 
-	
-	String Card1 = "Ace";
-	String Card2 = "1";
-	String Card3 = "2";
-	String Card4 = "3";
-	String Card5 = "4";
-	String Card6 = "6";
-	String Card7 = "7";
-	String Card8 = "8";
-	String Card9 = "9";
-	String Card10 = "10";
-	String Card11 = "Jack";
-	String Card12 = "Queen";
-	String card13 = "King";
-	
-	
-}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	Cards cards = new Cards();
+	Cards[] deck = new Cards[52];
+	String[] names = { "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king" };
+String [] suits = {"Spades","clubs","Hearts","diamond"};
+for (int x = 0;x<4;x++)
+	{String cd;
+	if (x < 2 ) cd = "black";
+	else cd = "red";
+	for (int y =0; y<13; y++) {
+		
+		
+	}
+
+	}
+
+}}
