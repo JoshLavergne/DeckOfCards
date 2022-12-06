69de7561309852ffaf627854ca2abd3c7caac2f8
Rarepug1st <99208797+Rarepug1st@users.noreply.github.com>
2022-12-06 14:37:01 -0500

    a

--- src/Cards.java
+++ src/Cards.java
@@ -11,11 +11,12 @@ public class Cards {
 		this.name = name;
 	}
 
-	Cards cards = new Cards();
+
 	Cards[] deck = new Cards[52];
 	String[] names = { "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king" };
-String [] suits = {"Spades","clubs","Hearts","diamond"};
-for (int x = 0;x<4;x++)
+    String [] suits = {"Spades","clubs","Hearts","diamond"};
+    
+    for (int x = 0;x<4;x++)
 	{String cd;
 	if (x < 2 ) cd = "black";
 	else cd = "red";
--- src/deck.java
+++ src/deck.java
@@ -3,6 +3,25 @@ public class deck {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
+		
+		
+		Cards[] deck = new Cards[52];
+		String[] names = { "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king" };
+	    String [] suits = {"Spades","clubs","Hearts","diamond"};
+	    
+	    for (int x = 0;x<4;x++)
+		{String cd;
+		if (x < 2 ) cd = "black";
+		else cd = "red";
+		for (int y =0; y<13; y++) {
+			
+			
+		}
+
+		}
 
 	}
 
