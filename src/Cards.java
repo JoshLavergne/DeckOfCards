b63823e06824fefef783ea380eb9cf90787072df
Rarepug1st <99208797+Rarepug1st@users.noreply.github.com>
2022-12-07 14:37:24 -0500

    ao

--- src/Cards.java
+++ src/Cards.java
@@ -1,16 +1,41 @@
 
 public class Cards {
+	private int value;
 	private String name;
+	private String suits;
+	private String colour;
 	// Lets use 52 Card
 
-	public String getName() {
-		return name;
+	public Cards(int i, String suits, String names, String colour) {
+		// TODO Auto-generated constructor stub
+		this.value = i;
+		this.suits =suits;
+	    this.name = names;
+	    this.colour = colour;
+	    
+	
 	}
 
-	public void setName(String name) {
-		this.name = name;
+	public String suits() {
+		return suits;
 	}
+	public String names() {
+		return names();
+	}
+	public String colour() {
+		return colour;
+		
+	}
+
+	public String printValue() {
+		return (this.name+" of "+this.suits+": "+this.colour);
+	}
+	}
+	
+
+	//public void setName(String name) {
+	//	this.name = name;
+	//}
 
 
 	
-}
--- src/deck.java
+++ src/deck.java
@@ -3,26 +3,34 @@ public class deck {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
+
 		Cards[] deck = new Cards[52];
 		String[] names = { "ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king" };
-	    String [] suits = {"Spades","clubs","Hearts","diamond"};
-	    
-	    for (int x = 0;x<4;x++)
-		{String cd;
-		if (x < 2 ) cd = "black";
-		else cd = "red";
-		for (int y =0; y<13; y++) {
-		deck [x*13+y] = new Cards (y+1, names[y],suits[x],cd);	
-			
-		}
+		String[] suits = { "Spades", "clubs", "Hearts", "diamond" };
 
+		for (int x = 0; x < 4; x++) {
+			String colour;
+			if (x < 2)
+				colour = "black";
+			else
+				colour = "red";
+			for (int y = 0; y < 13; y++) {
+				deck[x * 13 + y] = new Cards(y + 1, suits[x], names[y], colour);
+
+			}
 		}
+			Shuffle(deck);
+			for (Cards i : deck) {
+
+				System.out.println(i.printValue());
+
+			}
 //public static void Shuffle (Cards[] deck) {}
+	}
+
+	private static void Shuffle(Cards[] deck) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
